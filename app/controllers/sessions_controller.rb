1270f27f84ef3ee3ef149f60ef6737353f2ac2a4
porzingod <jasonlim.ny@gmail.com>
2018-03-05 17:46:12 -0500

    signin signout working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,31 @@
 class SessionsController < ApplicationController
+
+  def new
+    if !session[:user].nil?
+      redirect_to "/dashboard"
+    end
+  end
+
+  def create
+    if params[:password] || params[:password] != ""
+      @user = User.find_by(username: params[:username])
+      if @user.authenticate(params[:password])
+        session[:user] = @user.id
+        redirect_to "/dashboard"
+      else
+        render :new
+      end
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    if params
+      session.delete(:user)
+      redirect_to '/'
+    else
+      redirect_to '/'
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
 
     if @user.save
       session[:user] = @user.id
@@ -18,6 +18,12 @@ class UsersController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   private
